8a05333884393f9b8868ebe082361386816b4cf7
kev-mwangi <kevinmwas007@gmail.com>
2025-11-03 23:00:39 +0300

    add status

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,7 +3,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Header from './components/header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,6 +22,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <WelcomeMessage />
+      <Header/>
+      <MainContent/>
+      <Footer/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
