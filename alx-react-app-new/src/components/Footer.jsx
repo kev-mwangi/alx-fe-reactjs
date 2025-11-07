816248630bf354f054b03a6ae6a24a36c076f611
kev-mwangi <kevinmwas007@gmail.com>
2025-11-08 00:37:05 +0300

    add file

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer () {
+return (
+<footer style={{color:black,fontsize: Medium,}}>
+  <p>© 2023 City Lovers</p>
+</footer>
+);
+}
+export default Footer
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent () {
+return (
+<main>
+  <p style={{color:black,fontsize: Medium,}}>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+);
+}
+export default MainContent
