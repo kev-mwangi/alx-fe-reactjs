b72cfb81025efdb9987618674669280ed49b46ef
kev-mwangi <kevinmwas007@gmail.com>
2025-11-08 00:55:11 +0300

    add file

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,11 +3,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/header'
+import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
-
+import Counter from './components/Counter'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -27,6 +27,7 @@ function App() {
       <MainContent />
       <Footer />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
+      <Counter />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
