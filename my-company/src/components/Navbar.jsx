8a697b631b4012c02a5fdb62e13d14a767afc68c
kev-mwangi <kevinmwas007@gmail.com>
2025-12-18 21:11:52 +0300

    add status

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,15 +1,37 @@
-import { NavLink } from 'react-router-dom';
-import './Navbar.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav>
-      <NavLink to="/home">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/contact">Contact</NavLink>
-      <NavLink to="/service">Service</NavLink>
+    <nav 
+      style={{
+        backgroundColor: '#333',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '1rem 2rem',
+        color: 'white'
+      }}
+    >
+      <div style={{ fontSize: '1.5rem', fontWeight: 'bold' }}>
+        <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>
+          MyApp
+        </Link>
+      </div>
+      
+      <div style={{ display: 'flex', gap: '1rem' }}>
+        <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>
+          Home
+        </Link>
+        <Link to="/about" style={{ color: 'white', textDecoration: 'none' }}>
+          About
+        </Link>
+        <Link to="/contact" style={{ color: 'white', textDecoration: 'none' }}>
+          Contact
+        </Link>
+      </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
