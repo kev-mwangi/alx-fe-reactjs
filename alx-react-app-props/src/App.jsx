15f6dde56ce3ab5cc21ecee1bb0bde23d25ace04
kev-mwangi <kevinmwas007@gmail.com>
2025-12-18 20:54:08 +0300

    add status

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
